f207c224d315b8e1cce7595f00bd5c2eb9829a8e
Abel Cierto <u20201b561@upc.edu.pe>
2023-10-18 13:13:51 -0500

    implemented microservice shipment

--- shipment-microservice/src/main/java/com/softroute/shipmentmicroservice/entity/ShipmentEntity.java
+++ shipment-microservice/src/main/java/com/softroute/shipmentmicroservice/entity/ShipmentEntity.java
@@ -1,6 +1,5 @@
 package com.softroute.shipmentmicroservice.entity;
 
-import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,22 +22,22 @@ public class ShipmentEntity {
     @Column(name = "quantity", nullable = false)
     private int quantity;
     @Column(name = "date_registered", nullable = false)
-    private Date date_registered;
+    private Date dateRegistered;
     @Column(name = "code", nullable = false, length = 10, unique = true, updatable = false, columnDefinition = "varchar(10) default '0000000000'")
     private String code;
     @Column(name = "delivered_status", nullable = false, columnDefinition = "boolean default false")
-    private Boolean delivered_status;
+    private Boolean deliveredStatus;
     @Column(name = "origin_agency_id", nullable = false)
-    private Long origin_agency_id;
+    private Long originAgencyId;
     @Column(name = "destination_agency_id", nullable = false)
-    private Long destination_agency_id;
+    private Long destinationAgencyId;
     @Column(name = "type_package_id", nullable = false)
-    private Long type_package_id;
+    private Long typePackageId;
     @Column(name = "emitter_id", nullable = false)
-    private Long emitter_id;
+    private Long emitterId;
     @Column(name = "receiver_id", nullable = false)
-    private Long receiver_id;
+    private Long receiverId;
     @Column(name = "location_information_id", nullable = false)
-    private Long location_information_id;
+    private Long locationInformationId;
 
 }
--- shipment-microservice/src/main/java/com/softroute/shipmentmicroservice/repository/ShipmentRepository.java
+++ shipment-microservice/src/main/java/com/softroute/shipmentmicroservice/repository/ShipmentRepository.java
@@ -2,6 +2,17 @@ package com.softroute.shipmentmicroservice.repository;
 
 import com.softroute.shipmentmicroservice.entity.ShipmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ShipmentRepository extends JpaRepository<ShipmentEntity, Long> {
+    List<ShipmentEntity> getByEmitterId(Long emitterId);
+
+    List<ShipmentEntity> getByReceiverId(Long receiverId);
+
+    ShipmentEntity getByCode(String code);
+
+    ShipmentEntity getByEmitterIdAndReceiverId(Long emitterId, Long receiverId);
 }
