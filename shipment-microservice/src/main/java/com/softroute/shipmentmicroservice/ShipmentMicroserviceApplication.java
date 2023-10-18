2dea6f0f50bd1f8b035a5532e798bebc9c62d500
Abel Cierto <u20201b561@upc.edu.pe>
2023-10-18 16:35:56 -0500

    registered client and shipment microservice in eureka

--- shipment-microservice/src/main/java/com/softroute/shipmentmicroservice/ShipmentMicroserviceApplication.java
+++ shipment-microservice/src/main/java/com/softroute/shipmentmicroservice/ShipmentMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.softroute.shipmentmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShipmentMicroserviceApplication {
 
 	public static void main(String[] args) {
