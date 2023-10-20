3145995d0c770dfa55fc39445e3ea647ab867e01
Abel Cierto <u20201b561@upc.edu.pe>
2023-10-20 10:35:38 -0500

    feat: update method of client save and update

--- client-microservice/src/main/java/com/softroute/clientmicroservice/controller/ClientController.java
+++ client-microservice/src/main/java/com/softroute/clientmicroservice/controller/ClientController.java
@@ -2,18 +2,27 @@ package com.softroute.clientmicroservice.controller;
 
 import com.softroute.clientmicroservice.entity.ClientEntity;
 import com.softroute.clientmicroservice.service.ClientService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/client")
+@CrossOrigin(origins = "*")
+@Api(value = "Client Microservice", description = "This API has a CRUD for Client")
 public class ClientController {
     @Autowired
     ClientService clientService;
+
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<ClientEntity>> getAll() {
@@ -25,13 +34,28 @@ public class ClientController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ClientEntity> save(@RequestBody ClientEntity clientEntity) {
-        ClientEntity client = clientService.getByDni(clientEntity.getDni());
-        if(client != null) {
-            return ResponseEntity.ok(client);
+    @ApiOperation(value = "Save a new Client", notes = "This method save a new Client")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Client created successfully"),
+            @ApiResponse(code = 404, message = "Client not found"),
+            @ApiResponse(code = 501, message = "Internal Server Error")
+    })
+    public ResponseEntity<ClientEntity> save(@Valid @RequestBody ClientEntity clientEntity) {
+        try {
+            // Verificar si ya existe un usuario con la misma información
+            ClientEntity existingClient = clientService.getByDni(clientEntity.getDni()); // Suponiendo que el email sea un identificador único
+
+            if (existingClient != null) {
+                // El usuario ya existe en la base de datos, puedes devolver un error o tomar la acción apropiada.
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(existingClient); // HttpStatus.CONFLICT indica un conflicto (usuario ya registrado).
+            } else {
+                // El usuario no existe, así que puedes proceder a guardarlo.
+                ClientEntity clientNew = clientService.save(clientEntity);
+                return ResponseEntity.status(HttpStatus.CREATED).body(clientNew);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        ClientEntity clientNew = clientService.save(clientEntity);
-        return ResponseEntity.ok(clientNew);
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -49,8 +73,9 @@ public class ClientController {
         if(client == null) {
             return ResponseEntity.notFound().build();
         }
+
+        clientEntity.setId(client.getId());
         ClientEntity clientUpdated = clientService.update(clientEntity);
-        clientUpdated.setId(client.getId());
         if(clientUpdated == null) {
             return ResponseEntity.notFound().build();
         }
